4a210faaaa36634615851b785315be9a49051368
Camil <camil.hamdane@epfl.ch>
2018-10-29 14:48:51 +0100

    Project Structure (CMakeLists.txt and src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+  /* int errcode = 0;
+   try{
+   	
+   } catch(TCLAP::ArgException &e){
+   		std::cerr << "Error: " + e.error() + " " + e.argId();
+        errcode = 2;
+   }
+   return errcode;
+   */
+   return 0;
+}
